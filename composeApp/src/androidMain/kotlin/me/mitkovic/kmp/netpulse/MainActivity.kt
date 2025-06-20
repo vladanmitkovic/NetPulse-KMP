0b7e510bfaea9dd18c78f9fafeb522037dd72924
Vladan Mitkovic <vladan@endeavourtechnology.com>
2025-06-20 13:20:06 +0200

    NPKMP: Removing unused imports

--- composeApp/src/androidMain/kotlin/me/mitkovic/kmp/netpulse/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/me/mitkovic/kmp/netpulse/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import me.mitkovic.kmp.netpulse.ui.App
 
 class MainActivity : ComponentActivity() {
