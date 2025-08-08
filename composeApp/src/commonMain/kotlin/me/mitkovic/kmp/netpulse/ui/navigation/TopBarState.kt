0bd5c2dfc7609f198b7d484e31b5f45f8e5b3994
Vladan Mitkovic <vladan@endeavourtechnology.com>
2025-08-08 11:36:38 +0200

    NPKMP-23: Fixing app navigation

---
+++ composeApp/src/commonMain/kotlin/me/mitkovic/kmp/netpulse/ui/navigation/TopBarState.kt
@@ -0,0 +1,40 @@
+package me.mitkovic.kmp.netpulse.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.compose.currentBackStackEntryAsState
+import netpulse_kmp.composeapp.generated.resources.Res
+import netpulse_kmp.composeapp.generated.resources.app_name
+import netpulse_kmp.composeapp.generated.resources.history
+import netpulse_kmp.composeapp.generated.resources.settings
+import netpulse_kmp.composeapp.generated.resources.speed_test
+import org.jetbrains.compose.resources.stringResource
+
+data class TopBarState(
+    val title: String,
+    val showActions: Boolean,
+    val showBackIcon: Boolean,
+)
+
+@Composable
+fun NavController.currentTopBarState(): TopBarState {
+    val currentBackStackEntry by currentBackStackEntryAsState()
+    val currentDestination = currentBackStackEntry?.destination
+
+    val isHome = currentDestination?.hasRoute(Screen.Home::class) ?: true
+
+    return TopBarState(
+        title =
+            when {
+                isHome -> stringResource(Res.string.app_name)
+                currentDestination?.hasRoute(Screen.SpeedTest::class) == true -> stringResource(Res.string.speed_test)
+                currentDestination?.hasRoute(Screen.History::class) == true -> stringResource(Res.string.history)
+                currentDestination?.hasRoute(Screen.Settings::class) == true -> stringResource(Res.string.settings)
+                else -> ""
+            },
+        showActions = isHome,
+        showBackIcon = !isHome,
+    )
+}
