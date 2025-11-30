de0ecb2d7d5abbd1eb4137bec3eda4c102073eae
Vladan Mitkovic <vladan@endeavourtechnology.com>
2025-11-30 15:52:51 +0100

    NPKMP-30: Cleaning up the code

--- composeApp/src/desktopMain/kotlin/me/mitkovic/kmp/netpulse/di/PlatformModule.kt
+++ composeApp/src/desktopMain/kotlin/me/mitkovic/kmp/netpulse/di/PlatformModule.kt
@@ -4,23 +4,12 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
-import me.mitkovic.kmp.netpulse.data.local.ILocalStorage
-import me.mitkovic.kmp.netpulse.data.local.LocalStorageImpl
 import me.mitkovic.kmp.netpulse.data.local.database.NetPulseDatabase
-import me.mitkovic.kmp.netpulse.data.local.location.ILocationStorage
-import me.mitkovic.kmp.netpulse.data.local.location.LocationStorageImpl
-import me.mitkovic.kmp.netpulse.data.local.server.IServerStorage
-import me.mitkovic.kmp.netpulse.data.local.server.ServerStorageImpl
 import me.mitkovic.kmp.netpulse.data.local.settings.ISettingsDataStorage
 import me.mitkovic.kmp.netpulse.data.local.settings.SettingsDataStorageImpl
-import me.mitkovic.kmp.netpulse.data.local.testresult.ITestResultStorage
-import me.mitkovic.kmp.netpulse.data.local.testresult.TestResultStorageImpl
 import me.mitkovic.kmp.netpulse.data.local.theme.IThemeDataStorage
 import me.mitkovic.kmp.netpulse.data.local.theme.ThemeDataStorageImpl
-import me.mitkovic.kmp.netpulse.data.remote.IRemoteService
-import me.mitkovic.kmp.netpulse.data.remote.RemoteServiceImpl
 import me.mitkovic.kmp.netpulse.data.remote.installCommonPlugins
-import me.mitkovic.kmp.netpulse.logging.IAppLogger
 import nl.adaptivity.xmlutil.serialization.XML
 import org.koin.dsl.module
 
