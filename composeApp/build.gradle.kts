4dd056ac7986636f78a567c2a99794f074f42404
Vladan Mitkovic <vladan@endeavourtechnology.com>
2026-02-17 16:40:42 +0100

    NPKMP-34: Updating gradle to version 9

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,15 +1,13 @@
 import com.google.devtools.ksp.gradle.KspAATask
-import org.gradle.kotlin.dsl.withType
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidMultiplatformLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-    alias(libs.plugins.composeHotReload)
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.sqldelight)
     alias(libs.plugins.ksp)
@@ -22,9 +20,18 @@ kotlin {
         freeCompilerArgs.add("-Xexpect-actual-classes")
     }
 
-    androidTarget {
+    androidLibrary {
+        namespace = "me.mitkovic.kmp.netpulse.shared"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
+        }
+
+        androidResources {
+            enable = true
         }
     }
 
@@ -46,12 +53,12 @@ kotlin {
 
         commonMain.dependencies {
             // Compose & UI
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.ui)
+            implementation(libs.compose.components.resources)
+            implementation(libs.compose.components.uiToolingPreview)
 
             // AndroidX & Lifecycle
             implementation(libs.androidx.lifecycle.viewmodel)
@@ -87,17 +94,12 @@ kotlin {
             implementation(libs.kotlinx.datetime)
 
             // Icons
-            implementation(compose.materialIconsExtended)
+            implementation(libs.compose.material.iconsExtended)
         }
 
         androidMain.dependencies {
-            // Compose & UI
-            implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
-
             // Koin Android
             implementation(libs.koin.android)
-            implementation(libs.koin.androidx.compose)
 
             // Logging
             implementation(libs.timber)
@@ -128,9 +130,6 @@ kotlin {
 
             // SQLDelight Native Driver
             implementation(libs.sqldelight.native.driver)
-
-            // Duplicate? Consider removing if not needed
-            implementation(libs.ktor.client.darwin)
         }
 
         desktopMain.dependencies {
@@ -162,49 +161,9 @@ kotlin {
     }
 }
 
-android {
-    namespace = "me.mitkovic.kmp.netpulse"
-    compileSdk =
-        libs.versions.android.compileSdk
-            .get()
-            .toInt()
-
-    defaultConfig {
-        applicationId = "me.mitkovic.kmp.netpulse"
-        minSdk =
-            libs.versions.android.minSdk
-                .get()
-                .toInt()
-        targetSdk =
-            libs.versions.android.targetSdk
-                .get()
-                .toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    buildFeatures {
-        compose = true
-        buildConfig = true
-    }
-}
-
 dependencies {
     // Debug Tools
-    debugImplementation(compose.uiTooling)
+    androidRuntimeClasspath(libs.compose.ui.tooling)
 
     add("kspCommonMainMetadata", libs.koin.ksp.compiler)
     add("kspAndroid", libs.koin.ksp.compiler)
